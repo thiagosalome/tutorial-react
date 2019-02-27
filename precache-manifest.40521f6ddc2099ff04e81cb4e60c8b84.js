2b2ba5485a37b9be458a323c73fad9abc2d143f4
thiagosalome <thiagosalome@gmail.com>
2019-02-27 10:21:39 -0300

    Updates

---
+++ precache-manifest.40521f6ddc2099ff04e81cb4e60c8b84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5a39a0be60fe3b511780",
+    "url": "/static/js/main.5a39a0be.chunk.js"
+  },
+  {
+    "revision": "14bf863fa5da7fbc35b2",
+    "url": "/static/js/2.14bf863f.chunk.js"
+  },
+  {
+    "revision": "5a39a0be60fe3b511780",
+    "url": "/static/css/main.7ae496c6.chunk.css"
+  },
+  {
+    "revision": "07e0d35e61b693b62fb39bea2aae1e89",
+    "url": "/index.html"
+  }
+];
