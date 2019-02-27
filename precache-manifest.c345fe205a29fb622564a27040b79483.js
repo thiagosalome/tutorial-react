0881f9cb7026c735e25cc8302fdad50992183c46
thiagosalome <thiagosalome@gmail.com>
2019-02-27 12:49:47 -0300

    Updates

---
+++ precache-manifest.c345fe205a29fb622564a27040b79483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "d5a8aa10bb4d836ca8f8",
+    "url": "./static/js/main.d5a8aa10.chunk.js"
+  },
+  {
+    "revision": "14bf863fa5da7fbc35b2",
+    "url": "./static/js/2.14bf863f.chunk.js"
+  },
+  {
+    "revision": "d5a8aa10bb4d836ca8f8",
+    "url": "./static/css/main.7ae496c6.chunk.css"
+  },
+  {
+    "revision": "5ea6d3df5be294445795c30699a7f8ea",
+    "url": "./index.html"
+  }
+];
